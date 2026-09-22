aa2fb52f66cf49a8e752dc92a2ce59d81c85adb3
KG <ganjar.satyanagara@gmail.com>
2026-09-22 16:49:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
 //   Apps Script > Deploy > Manage deployments > klik ikon pensil (EDIT deployment yang sudah ada)
 //   > Version: New version > Deploy. URL TETAP.
 // JANGAN klik "New deployment" — itu membuat URL BARU dan URL lama mati.
-var API_URL = 'https://script.google.com/macros/s/AKfycbwus1oK3oQC6XYnfwjRaCDBQytl0HDB6ZjuCWNuUPAfj7cg6e7Ebnk3Op94vIU6W42/exec';
+var API_URL = 'https://script.google.com/macros/s/AKfycbwus1oK3oQC6XYnfwjRaCDBQytl0HDB6ZjuCWNUuPQAfj7cg6e7Ebnk3Op94vIU6W42/exec';
